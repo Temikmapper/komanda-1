286e16d60b59bb0a4e54ca991b5f88a5d605f92b
tema <dovolniyzavodchanin@yandex.ru>
2022-05-16 10:11:20 +0300

    added ability to view spends per current month

--- komanda/main/urls.py
+++ komanda/main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('test_base', views.test_base, name='test_base'),
     path('all', views.show_all, name='show_all'),
     path('add', views.add_spend, name='add_spend'),
-    path('delete/<int:id>', views.delete_category, name='delete_category')
+    path('delete/<int:id>', views.delete_category, name='delete_category'),
+    path('monthly/<int:year>/<int:month>', views.monthly, name='monthly')
 ]
--- komanda/main/views.py
+++ komanda/main/views.py
@@ -1,10 +1,15 @@
 from datetime import datetime, timedelta
-from pydoc import describe
-from unicodedata import category
 from django.shortcuts import render, redirect
 from main.models import Spendings, Categories
 from main.forms import SpendForm, CategoryAddForm
 
+def get_curent_date():
+
+    date = {'year': datetime.today().year,
+            'month': datetime.today().month}
+    return date
+
+CURRENT_DATE = get_curent_date()
 
 def index(request):
     categories = Categories.objects.all()
@@ -15,15 +20,15 @@ def index(request):
             category.save()
     else:
         form = CategoryAddForm()
-    return render(request, 'index.html', {'form': form, 'categories': categories})
+    return render(request, 'index.html', {'form': form, 'categories': categories, 'date': CURRENT_DATE})
     
 
 def test_base(request):
     return render(request, 'base.html')
 
 def show_all(request):
-    data = {'days': Spendings.objects.all()}
-    return render(request, 'all.html', data)
+    data = Spendings.objects.all()
+    return render(request, 'all.html', {'days': data, 'date': CURRENT_DATE})
 
 def add_spend(request):
     if request.method == "POST":
@@ -33,9 +38,18 @@ def add_spend(request):
             spend.save()
     else:
         form = SpendForm()
-    return render(request, 'add.html', {'form': form})
+    return render(request, 'add.html', {'form': form, 'date': CURRENT_DATE})
 
 def delete_category(request, id):
     category = Categories.objects.get(id=id)
     category.delete()
     return redirect('index')
+
+def monthly(request, year, month):
+
+    start_of_month = datetime(year, month, 1)
+    end_of_month = datetime(year, month+1, 1) - timedelta(1)
+
+    data = Spendings.objects.filter(date__gte=start_of_month).filter(date__lte=end_of_month).order_by('date')
+
+    return render(request, 'monthly.html', {'date': CURRENT_DATE, 'days': data})
