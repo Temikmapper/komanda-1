c1e0e002035bfdf7df8906ee4f7f37e8ae0894ad
tema <dovolniyzavodchanin@yandex.ru>
2022-05-16 11:24:43 +0300

    added tabs for monthly view

--- komanda/main/urls.py
+++ komanda/main/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('all', views.show_all, name='show_all'),
     path('add', views.add_spend, name='add_spend'),
     path('delete/<int:id>', views.delete_category, name='delete_category'),
-    path('monthly/<int:year>/<int:month>', views.monthly, name='monthly')
+    path('monthly/<int:year>/<int:month>', views.monthly, name='monthly'),
 ]
--- komanda/main/views.py
+++ komanda/main/views.py
@@ -1,7 +1,9 @@
+from calendar import monthrange
 from datetime import datetime, timedelta
 from django.shortcuts import render, redirect
 from main.models import Spendings, Categories
 from main.forms import SpendForm, CategoryAddForm
+
 
 def get_curent_date():
 
@@ -9,7 +11,9 @@ def get_curent_date():
             'month': datetime.today().month}
     return date
 
+
 CURRENT_DATE = get_curent_date()
+
 
 def index(request):
     categories = Categories.objects.all()
@@ -21,14 +25,16 @@ def index(request):
     else:
         form = CategoryAddForm()
     return render(request, 'index.html', {'form': form, 'categories': categories, 'date': CURRENT_DATE})
-    
+
 
 def test_base(request):
     return render(request, 'base.html')
 
+
 def show_all(request):
     data = Spendings.objects.all()
     return render(request, 'all.html', {'days': data, 'date': CURRENT_DATE})
+
 
 def add_spend(request):
     if request.method == "POST":
@@ -40,16 +46,51 @@ def add_spend(request):
         form = SpendForm()
     return render(request, 'add.html', {'form': form, 'date': CURRENT_DATE})
 
+
 def delete_category(request, id):
     category = Categories.objects.get(id=id)
     category.delete()
     return redirect('index')
 
-def monthly(request, year, month):
+
+def monthly(request, year, month): #TODO рефакторить
+    month_names = {1: 'January',
+                   2: 'February',
+                   3: 'March',
+                   4: 'April',
+                   5: 'May',
+                   6: 'June',
+                   7: 'July',
+                   8: 'August',
+                   9: 'September',
+                   10: 'October',
+                   11: 'November',
+                   12: 'December'}
+    jan = {'id':1, 'name':'January'}
+    feb = {'id':2, 'name':'February'}
+    mar = {'id':3, 'name':'March'}
+    apr = {'id':4, 'name':'April'}
+    may = {'id':5, 'name':'May'}
+    jun = {'id':6, 'name':'June'}
+    jul = {'id':7, 'name':'July'}
+    aug = {'id':8, 'name':'August'}
+    sep = {'id':9, 'name':'September'}
+    oct = {'id':10, 'name':'October'}
+    nov = {'id':11, 'name':'November'}
+    dec = {'id':12, 'name':'Decemeber'}
+    monthes = [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec]
+
+    last_day = monthrange(year, month)[1]
 
     start_of_month = datetime(year, month, 1)
-    end_of_month = datetime(year, month+1, 1) - timedelta(1)
+    end_of_month = datetime(year, month, last_day)
 
-    data = Spendings.objects.filter(date__gte=start_of_month).filter(date__lte=end_of_month).order_by('date')
+    data = Spendings.objects.filter(date__gte=start_of_month).filter(
+        date__lte=end_of_month).order_by('date')
 
-    return render(request, 'monthly.html', {'date': CURRENT_DATE, 'days': data})
+    return render(request, 'monthly.html',
+                  {'date': CURRENT_DATE,
+                   'days': data,
+                   'year': year,
+                   'monthes': monthes,
+                   'cur_month': month_names[month]})
