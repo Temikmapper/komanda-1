8142b3ef725e2da5b6582511e2925be8ed9539a4
tema <dovolniyzavodchanin@yandex.ru>
2022-05-18 12:45:16 +0300

    added auth functionality, fixed styling

--- komanda/main/urls.py
+++ komanda/main/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+from django.contrib.auth import views as views_auth
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('test_base', views.test_base, name='test_base'),
@@ -13,5 +15,7 @@ urlpatterns = [
     path('monthly/<int:year>/<int:month>/raw', views.monthly_raw, name='monthly_raw'),
     path('monthly/<int:year>/<int:month>/income', views.monthly_income, name='monthly_income'),
     path('monthly/<int:year>/<int:month>/income/<int:id>/edit', views.income_edit, name='income_edit'),
-    path('income/delete/<int:id>', views.income_delete, name='income_delete')
+    path('income/delete/<int:id>', views.income_delete, name='income_delete'),
+    path('accounts/login/', views_auth.LoginView.as_view(next_page='/'), name='login'),
+    path('accounts/logout/', views_auth.LogoutView.as_view(next_page='/'), name='logout')
 ]
--- komanda/main/views.py
+++ komanda/main/views.py
@@ -3,18 +3,20 @@ from calendar import monthrange
 from datetime import datetime, timedelta
 from decimal import ROUND_05UP, ROUND_CEILING, ROUND_FLOOR, ROUND_HALF_EVEN, Decimal
 from django.shortcuts import render, redirect
+from django.db.models import Sum
+from django.contrib.auth.decorators import login_required
 from main.models import Spendings, Categories, Incomes
 from main.forms import SpendForm, CategoryAddForm, IncomeEditForm, IncomeAddForm
 
 
-def get_curent_date():
+def get_current_date():
 
     date = {'year': datetime.today().year,
             'month': datetime.today().month}
     return date
 
 
-CURRENT_DATE = get_curent_date()
+CURRENT_DATE = get_current_date()
 MONTH_NAMES = {1: 'January',
                2: 'February',
                3: 'March',
@@ -27,6 +29,20 @@ MONTH_NAMES = {1: 'January',
                10: 'October',
                11: 'November',
                12: 'December'}
+
+jan = {'id': 1, 'name': 'January'}
+feb = {'id': 2, 'name': 'February'}
+mar = {'id': 3, 'name': 'March'}
+apr = {'id': 4, 'name': 'April'}
+may = {'id': 5, 'name': 'May'}
+jun = {'id': 6, 'name': 'June'}
+jul = {'id': 7, 'name': 'July'}
+aug = {'id': 8, 'name': 'August'}
+sep = {'id': 9, 'name': 'September'}
+oct = {'id': 10, 'name': 'October'}
+nov = {'id': 11, 'name': 'November'}
+dec = {'id': 12, 'name': 'Decemeber'}
+MONTHES = [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec]
 
 
 def index(request):
@@ -44,12 +60,12 @@ def index(request):
 def test_base(request):
     return render(request, 'base.html')
 
-
+@login_required
 def show_all(request):
     data = Spendings.objects.all()
     return render(request, 'all.html', {'days': data, 'date': CURRENT_DATE})
 
-
+@login_required
 def add_spend(request):
     if request.method == "POST":
         form = SpendForm(request.POST)
@@ -60,66 +76,47 @@ def add_spend(request):
         form = SpendForm()
     return render(request, 'add.html', {'form': form, 'date': CURRENT_DATE})
 
-
+@login_required
 def delete_category(request, id):
     category = Categories.objects.get(id=id)
     category.delete()
     return redirect('index')
 
-
+@login_required
 def delete_expense(request, id, year, month):
     expense = Spendings.objects.get(id=id)
     expense.delete()
     return redirect('index')  # TODO редикрет обратно в raw
 
-
+@login_required
 def monthly(request, year, month):  # TODO рефакторить
-    jan = {'id': 1, 'name': 'January'}
-    feb = {'id': 2, 'name': 'February'}
-    mar = {'id': 3, 'name': 'March'}
-    apr = {'id': 4, 'name': 'April'}
-    may = {'id': 5, 'name': 'May'}
-    jun = {'id': 6, 'name': 'June'}
-    jul = {'id': 7, 'name': 'July'}
-    aug = {'id': 8, 'name': 'August'}
-    sep = {'id': 9, 'name': 'September'}
-    oct = {'id': 10, 'name': 'October'}
-    nov = {'id': 11, 'name': 'November'}
-    dec = {'id': 12, 'name': 'Decemeber'}
-    monthes = [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec]
 
     last_day = monthrange(year, month)[1]
 
     start_of_month = datetime(year, month, 1)
     end_of_month = datetime(year, month, last_day)
 
+    monthly_income = get_monthly_income(
+        start_of_month).quantize(Decimal("1.00"), ROUND_FLOOR)
+    daily_income = get_daily_income(start_of_month, end_of_month).quantize(
+        Decimal("1.00"), ROUND_FLOOR)
+    total_expenses = Spendings.objects.filter(date__gte=start_of_month).filter(
+        date__lte=end_of_month).aggregate(Sum("amount"))['amount__sum']
     data = get_balance_for_monthly_table(start_of_month, end_of_month)
 
     return render(request, 'monthly.html',
                   {'date': CURRENT_DATE,
-                   'days': data[0],
-                   'daily_income': data[1],
-                   'monthly_income': data[2],
+                   'days': data,
+                   'daily_income': daily_income,
+                   'monthly_income': monthly_income,
+                   'total_expenses': total_expenses,
                    'year': year,
                    'month': month,
-                   'monthes': monthes,
+                   'monthes': MONTHES,
                    'cur_month': MONTH_NAMES[month]})
 
-
+@login_required
 def monthly_raw(request, year, month):
-    jan = {'id': 1, 'name': 'January'}
-    feb = {'id': 2, 'name': 'February'}
-    mar = {'id': 3, 'name': 'March'}
-    apr = {'id': 4, 'name': 'April'}
-    may = {'id': 5, 'name': 'May'}
-    jun = {'id': 6, 'name': 'June'}
-    jul = {'id': 7, 'name': 'July'}
-    aug = {'id': 8, 'name': 'August'}
-    sep = {'id': 9, 'name': 'September'}
-    oct = {'id': 10, 'name': 'October'}
-    nov = {'id': 11, 'name': 'November'}
-    dec = {'id': 12, 'name': 'Decemeber'}
-    monthes = [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec]
 
     last_day = monthrange(year, month)[1]
 
@@ -134,10 +131,10 @@ def monthly_raw(request, year, month):
                    'days': data,
                    'year': year,
                    'month': month,
-                   'monthes': monthes,
+                   'monthes': MONTHES,
                    'cur_month': MONTH_NAMES[month]})
 
-
+@login_required
 def monthly_income(request, year, month):
     date = datetime(year, month, 1)
 
@@ -164,7 +161,7 @@ def monthly_income(request, year, month):
                    'incomes': incomes,
                    'form': form})
 
-
+@login_required
 def income_edit(request, year, month, id):
     income = Incomes.objects.get(id=id)
 
@@ -183,21 +180,35 @@ def income_edit(request, year, month, id):
                    'form': form,
                    })
 
+@login_required
 def income_delete(request, id):
     income = Incomes.objects.get(id=id)
     income.delete()
     return redirect('index')
 
 
+def get_monthly_income(date):
+    total_monthly_income = Incomes.objects.filter(
+        date=date).aggregate(Sum("value"))
+    if total_monthly_income['value__sum'] == None:
+        return Decimal(0.00)
+    return total_monthly_income['value__sum']
+
+
+def get_daily_income(start, finish):
+    monthly_income = get_monthly_income(start)
+    num_of_days = abs((finish-start).days+1)
+    daily_income = Decimal(monthly_income/num_of_days)
+    return daily_income
+
+
 def get_balance_for_monthly_table(start_of_month, end_of_month):
     cur_day = start_of_month
     data = []
     accumulated = Decimal(0)
-    montly_income = Decimal(2000)  # TODO fromDB
-    num_of_days = (end_of_month-start_of_month).days+1
+    daily_income = get_daily_income(start_of_month, end_of_month)
     accumulated_income = Decimal(0)
     accumulated_balance = Decimal(0)
-    daily_income = Decimal(montly_income/num_of_days)
 
     while (cur_day != end_of_month+timedelta(1)):
         spends_in_day = Spendings.objects.filter(date=cur_day)
@@ -222,4 +233,4 @@ def get_balance_for_monthly_table(start_of_month, end_of_month):
                          'accumulated_balance': accumulated_balance.quantize(Decimal("1.00"), ROUND_FLOOR)})
         cur_day += timedelta(1)
 
-    return data, daily_income.quantize(Decimal("1.00"), ROUND_FLOOR), montly_income.quantize(Decimal("1.00"), ROUND_FLOOR)
+    return data
