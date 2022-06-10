c9e30ae76348860126172552dd043ea97206aef1
tema <dovolniyzavodchanin@yandex.ru>
2022-06-10 10:59:21 +0300

    unittests for goals models

--- komanda/goals/models.py
+++ komanda/goals/models.py
@@ -1,35 +1,57 @@
 from datetime import datetime
-from decimal import Decimal
+from decimal import ROUND_FLOOR, Decimal
 from django.db import models
 from django.utils import timezone
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
 
 
 class Goals(models.Model):
     name = models.CharField(max_length=50)
     date = models.DateField()
     value = models.DecimalField(max_digits=13, decimal_places=2)
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        GoalStatus.objects.create(date=datetime.today(), value=Decimal(0.0), percent=Decimal(0.0), goal=self)
+    accumulated = models.DecimalField(
+        max_digits=13, decimal_places=2, default=Decimal(0)
+    )
 
     def __str__(self):
         return self.name
 
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        GoalStatus.objects.create(
+            date=datetime.today(), value=Decimal(0), percent=Decimal(0), goal=self
+        )
+
+    def bump(self, value, date):
+        self.accumulated += value
+        return GoalStatus.objects.create(date=date, value=value, goal=self)
+
+    def get_left(self):
+        return (self.value - self.accumulated).quantize(Decimal("1.00"), ROUND_FLOOR)
+
+    def get_percent(self):
+        percent = (self.get_left() / self.value) * 100
+        return percent.quantize(Decimal("1.00"), ROUND_FLOOR)
+
+    def get_accumulated(self):
+        return self.accumulated.quantize(Decimal("1.00"), ROUND_FLOOR)
+
+    def get_history_list(self):
+        statuses = GoalStatus.objects.filter(goal=self)
+        return list(statuses)
+
 
 class GoalStatus(models.Model):
     date = models.DateField(default=timezone.now)
-    value = models.DecimalField(max_digits=13, decimal_places=2, default=Decimal(00.00))
-    percent = models.DecimalField(
-        max_digits=4, decimal_places=2, default=Decimal(00.00)
-    )
+    value = models.DecimalField(max_digits=13, decimal_places=2, default=Decimal(0))
+    percent = models.DecimalField(max_digits=4, decimal_places=2, default=Decimal(0))
     goal = models.ForeignKey(Goals, on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ["date"]
+        ordering = ["date", "id"]
+
+    def __str__(self) -> str:
+        string = f'id: {self.id}, date: {self.date}'
+        return string
 
     def left(self):
 
--- komanda/goals/tests/test_models.py
+++ komanda/goals/tests/test_models.py
@@ -8,24 +8,59 @@ from goals.models import Goals, GoalStatus
 class GoalsModelTest(TestCase):
     """Тестирование моделей для целей"""
 
+    def setUp(self):
+        global goal
+        goal = Goals.objects.create(
+            name="car", date=date(2021,12,31), value=Decimal(10.0)
+        )
+
+    def tearDown(self):
+        goal.delete()
+
     def test_goal_prints_its_name(self):
-        goal = Goals.objects.create(name="car", date=datetime.today(), value=Decimal(10.0))
         self.assertEqual(goal.name, str(goal))
 
     def test_goal_creates_history_at_creation(self):
-        goal = Goals.objects.create(name="car", date=datetime.today(), value=Decimal(10.0))
         history_object = GoalStatus.objects.filter(goal=goal)
         self.assertEqual(len(history_object), 1)
 
-    def test_creates_status_after_bump(self):
-        """тест: пополнение цели
-        """
-        goal = Goals.objects.create(name="car", date=datetime.today(), value=Decimal(10.0))
-        goal.bump(Decimal(1.0), date=date(2022,1,1))
-        last_status = GoalStatus.objects.last()
-        self.assertEqual(last_status.value, Decimal(1.0))
+    def test_goal_get_left(self):
+        """тест: получение сколько осталось накопить"""
+        goal.bump(Decimal(5.0), date=date(2022, 1, 1))
+        goal.bump(Decimal(1.0), date=date(2022, 1, 1))
+        self.assertEqual(goal.get_left(), Decimal(4.0))
 
-    def test_goal_get_current_value(self):
-        goal = Goals.objects.create(name="car", date=datetime.today(), value=Decimal(10.0))
-        goal.bump(Decimal(5.0))
-        self.assertEqual(goal.left, Decimal(5.0))
+    def test_goal_returns_percent(self):
+        """тест: цель возвращает сколько накоплено в процентах"""
+        goal.bump(Decimal(5.0), date=date(2022, 1, 1))
+        percent = goal.get_percent()
+        self.assertEqual(percent, Decimal(50.0))
+
+    def test_goal_returns_accumulated(self):
+        """тест: цель возращает сколько накоплено"""
+        goal.bump(Decimal(3.0), date=date(2022, 1, 1))
+        goal.bump(Decimal(4.0), date=date(2022, 1, 2))
+        accumulated = goal.get_accumulated()
+        self.assertEqual(accumulated, Decimal(7.0))
+
+    def test_goal_return_beautiful_string(self):
+        """тест: при обращении к методам цели, они возвращают десятичные с двумя знаками после запятой"""
+        goal.bump(Decimal(5.0), date=date(2022, 1, 1))
+        percent = str(goal.get_percent())
+        left = str(goal.get_left())
+        accumulated = str(goal.get_accumulated())
+        self.assertEqual(accumulated, "5.00")
+        self.assertEqual(percent, "50.00")
+        self.assertEqual(left, "5.00")
+
+    def test_goal_returns_its_history_in_right_order(self):
+        """тест: цель возвращает свою историю в правильном порядке
+        """
+        bump1 = goal.bump(Decimal(1.0), date=date(2023, 1, 1))
+        bump2 = goal.bump(Decimal(2.0), date=date(2023, 1, 1))
+        bump4 = goal.bump(Decimal(5.0), date=date(2023, 1, 5))
+        bump3 = goal.bump(Decimal(1.0), date=date(2023, 1, 2))
+
+        goal_history_list = goal.get_history_list()[1:]
+        right_order = [bump1, bump2, bump3, bump4]
+        self.assertEqual(goal_history_list, right_order)
