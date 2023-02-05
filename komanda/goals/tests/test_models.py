43126d8ddebd6f74fa308c1f28bc947c939fb9bf
Artem Sherstyuk <sher-art@ya.ru>
2023-02-05 13:13:59 +0300

    feat KOM-83: добавлена возможность получить сумму трат и поступлений за месяц, используется это теперь в Табло

--- komanda/goals/tests/test_models.py
+++ komanda/goals/tests/test_models.py
@@ -82,8 +82,7 @@ class GoalsModelTest(TestCase):
         self.assertEqual(percent, "50.00")
         self.assertEqual(left, "5.00")
 
-    @skip
-    def test_goal_returns_its_history_in_right_order(self):
+    def test_goal_returns_its_expenses_history_in_right_order(self):
         """тест: цель возвращает свою историю в правильном порядке"""
         goal = Goals.objects.all()[0]
         bump1 = goal.add_expense(Decimal(1.0), date=date(2023, 1, 1))
@@ -91,7 +90,7 @@ class GoalsModelTest(TestCase):
         bump4 = goal.add_expense(Decimal(5.0), date=date(2023, 1, 5))
         bump3 = goal.add_expense(Decimal(1.0), date=date(2023, 1, 2))
 
-        goal_history_list = goal.get_history_list()[1:]
+        goal_history_list = goal.get_expenses_list()
         right_order = [bump1, bump2, bump3, bump4]
         self.assertEqual(goal_history_list, right_order)
 
@@ -137,3 +136,24 @@ class GoalsModelTest(TestCase):
         self.assertEqual(balance_by_february, Decimal(80))
         balance_by_2022 = goal.get_balance_by_month(year=2022, month=12)
         self.assertEqual(balance_by_2022, Decimal(0))
+
+    def test_get_expenses_value_in_month(self):
+        goal = Goals.objects.create(
+            name="rocket", date=date(2023, 12, 31), value=Decimal(10000.0)
+        )
+
+        GoalExpense.objects.create(
+            goal=goal, value=Decimal(10.0), date=date(2023, 1, 31)
+        )
+        GoalExpense.objects.create(
+            goal=goal, value=Decimal(30.0), date=date(2023, 1, 15)
+        )
+        GoalExpense.objects.create(
+            goal=goal, value=Decimal(200.0), date=date(2023, 1, 15)
+        )
+
+        result_at_jan = goal.get_expenses_value_in_month(2023, 1)
+        result_at_feb = goal.get_expenses_value_in_month(2023, 2)
+
+        self.assertEqual(result_at_jan, Decimal(240))
+        self.assertEqual(result_at_feb, 0)
